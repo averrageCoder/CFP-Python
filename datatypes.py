0692cfdd0f0dd30838f6701ecfcbc85d50ef69fc
Harsh R Jain <rjainharsh@gmail.com>
2021-10-28 16:29:24 +0530

    [Harsh]ADDED: program to understand different python datatypes

---
+++ datatypes.py
@@ -0,0 +1,11 @@
+noneDataType = None
+booleanDataType = True
+numberDataType = 34
+realNumberDataType = 23.34
+stringDataType = "Hello"
+
+print("Type: ", type(noneDataType), " Value: ", noneDataType)
+print("Type: ", type(booleanDataType), " Value: ", booleanDataType)
+print("Type: ", type(numberDataType), " Value: ", numberDataType)
+print("Type: ", type(realNumberDataType), " Value: ", realNumberDataType)
+print("Type: ", type(stringDataType), " Value: ", stringDataType)
