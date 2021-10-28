4180d12d147f2a5a3158b72a702a4092cc593dff
Harsh R Jain <rjainharsh@gmail.com>
2021-10-28 16:29:06 +0530

    [Harsh]ADDED: program to understand python variables

---
+++ variables.py
@@ -0,0 +1,13 @@
+variable_string = "24"
+variable_integer = 24
+variable_float = 2.34
+
+x = y = z = 0
+a, b, c = None, None, None
+
+print("Printing a String\t:", variable_string)
+print("Printing a String\t: %s" % variable_string)
+print("Printing a Number\t: %d" % variable_integer)
+print("Printing a Decimal\t: %f" % variable_float)
+print("Multiple Formats\t: %s, %d, %f" % (variable_string, variable_integer, variable_float))
+print("Multiple Formats\t: %s, %d, %f" % (variable_string, variable_integer, variable_float))
