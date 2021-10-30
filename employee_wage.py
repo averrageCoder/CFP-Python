3fba21ff89af34c5658221c6b75049940e11769b
Harsh R Jain <rjainharsh@gmail.com>
2021-10-30 19:30:44 +0530

    [Harsh]ADDED: ability to calculate daily employee wage

--- employee_wage.py
+++ employee_wage.py
@@ -1,12 +1,17 @@
 import random
 
-"""
-
-"""
-
 IS_PRESENT = 1
 IS_ABSENT = 0
+WAGE_PER_HOUR = 20
+FULL_DAY_HOUR = 8
+
+# UC1
 if random.randint(0, 1) == IS_PRESENT:
     print("Employee is present")
+    daily_wage = FULL_DAY_HOUR * WAGE_PER_HOUR
 else:
     print("Employee is absent!")
+    daily_wage = 0
+
+# UC2
+print("Employee Wage: {}".format(daily_wage))
