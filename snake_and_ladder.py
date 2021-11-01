abaf6d9e8ebb1f667d9d5aa85a7ce24447bcbe3d
Harsh R Jain <rjainharsh@gmail.com>
2021-11-01 16:04:00 +0530

    [Harsh] ADDED: ability to play the same with 2 players

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -5,11 +5,17 @@ NO_PLAY_OPTION = 0
 LADDER_OPTION = 1
 SNAKE_OPTION = 2
 
-player_position = START_POSITION
-dice_rolls = 0
-while player_position < 100:
+player1_position = START_POSITION
+player2_position = START_POSITION
+player1_chance = True
+
+while player1_position < 100 and player2_position < 100:
+    if player1_chance:
+        player_position = player1_position
+    else:
+        player_position = player2_position
+
     dice_number = random.randint(1, 6)
-    dice_rolls += 1
     option = random.randint(0, 2)
     if option == NO_PLAY_OPTION:
         player_position = player_position
@@ -21,6 +27,20 @@ while player_position < 100:
         player_position -= dice_number
         if player_position < 0:
             player_position = 0
-    print("Dice roll: {} Current position: {}".format(dice_rolls, player_position))
 
-print("Position in the end = {} with total {} dice rolls".format(player_position, dice_rolls))
+    if player1_chance:
+        player1_position = player_position
+    else:
+        player2_position = player_position
+
+    print("Player 1 position is {} and player 2 position is {}".format(player1_position, player2_position))
+    if option != LADDER_OPTION:
+        if player1_chance:
+            player1_chance = False
+        else:
+            player1_chance = True
+
+if player1_position == 100:
+    print("Player 1 has won!")
+else:
+    print("Player 2 has won!")
