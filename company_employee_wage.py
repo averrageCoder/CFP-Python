f24d6d215cf1332d55f3ab59b4493624b2063922
Harsh R Jain <rjainharsh@gmail.com>
2021-11-01 15:42:32 +0530

    [Harsh] REFACTORED: variable name and added doc string

--- company_employee_wage.py
+++ company_employee_wage.py
@@ -1,26 +1,61 @@
 class CompanyEmployeeWage:
-    def __init__(self, company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth):
+    """
+    Class to store company employee wage
+
+    Attributes:
+        company: name of the company
+        emp_rate_per_hour: employee rate per hour
+        num_of_working_days: max number of working days
+        max_hours_per_month: max hours per month
+    """
+
+    def __init__(self, company, emp_rate_per_hour, num_of_working_days, max_hours_per_month):
+        """
+        Constructor to initialize the object
+        :param company: name of the company
+        :param emp_rate_per_hour: employee rate per hour
+        :param num_of_working_days: max number of working days
+        :param max_hours_per_month: max hours per month
+        """
         self.company = company
-        self.empRatePerHour = empRatePerHour
-        self.numOfWorkingDays = numOfWorkingDays
-        self.maxHoursPerMonth = maxHoursPerMonth
+        self.emp_rate_per_hour = emp_rate_per_hour
+        self.num_of_working_days = num_of_working_days
+        self.max_hours_per_month = max_hours_per_month
 
-    def setTotalEmpWage(self, totalEmpWage):
-        self.totalEmpWage = totalEmpWage
+    def set_total_emp_wage(self, total_emp_wage):
+        """
+        Method to set the total employee wage
+        :param total_emp_wage: total employee wage
+        :return: None
+        """
+        self.total_emp_wage = total_emp_wage
 
-    def getTotalWage(self):
+    def get_total_wage(self):
+        """
+        Method to return the total employee wage
+        :return: total employee wage
+        """
         try:
-            return self.totalEmpWage
+            return self.total_emp_wage
         except Exception as e:
             print(e.__str__())
             return 0
 
-    def setDailyWageDict(self, dailyWages):
-        self.dailyWages = dailyWages
+    def set_daily_wage_dict(self, daily_wages):
+        """
+        Method to store daily wages mappings
+        :param daily_wages: diction with daily wages
+        :return: None
+        """
+        self.daily_wages = daily_wages
 
     def __str__(self):
+        """
+        String version of the class object
+        :return: object details as a string
+        """
         string = ""
-        for key, value in self.dailyWages.items():
+        for key, value in self.daily_wages.items():
             string += "\n Day: {} Wage: {}".format(key, value)
-        string += "\n Total employee wage for company: " + str(self.company) + " is: " + str(self.totalEmpWage)
+        string += "\n Total employee wage for company: " + str(self.company) + " is: " + str(self.total_emp_wage)
         return string
