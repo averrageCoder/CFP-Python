e22747e2a0c2594668f0dc7afd3ce95b787bec92
Harsh R Jain <rjainharsh@gmail.com>
2021-11-02 16:43:20 +0530

    reset

--- address_book_system.py
+++
@@ -1,8 +0,0 @@
-from contact import Contact
-
-if __name__ == "__main__":
-    address_books = []
-    contact = Contact("James", "William", "12th Street", "London", "London",
-                          "789456", "7894561230", "james@james.com")
-    address_books.append(contact)
-    print(contact)
