0acfb2f9bd12383d6a143dd4d21d9588078a2e7c
Harsh R Jain <rjainharsh@gmail.com>
2021-11-10 18:23:07 +0530

    [Harsh] ADDED: tests to validate phone

--- user_registeration_main.py
+++ user_registeration_main.py
@@ -16,3 +16,7 @@ def test_email():
     assert not user_registeration.validate_email("jaja.com")
     assert user_registeration.validate_email("abc.xyz@bl.co.in")
 
+
+def test_phone():
+    assert not user_registeration.validate_phone("789 456 1232")
+    assert user_registeration.validate_phone("+91 9874561230")
