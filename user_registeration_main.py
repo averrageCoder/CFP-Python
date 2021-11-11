ac01c6d09a53c9f6becde81f33c9e8ef65917fa0
Harsh R Jain <rjainharsh@gmail.com>
2021-11-11 08:53:41 +0530

    [Harsh] ADDED: parameterized tests for email validation

--- user_registeration_main.py
+++ user_registeration_main.py
@@ -1,6 +1,23 @@
 import pytest
 import user_registeration
 
+list_of_emails = []
+correct_emails = [
+    "abc-100@yahoo.com", "abc@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+    "abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com"
+ ]
+incorrect_emails = [
+    "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com",
+    "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a",
+    "abc@gmail.com.aa.au"
+]
+
+for email in correct_emails:
+    list_of_emails.append((email, True))
+
+for email in incorrect_emails:
+    list_of_emails.append((email, False))
+
 
 def test_first_name():
     assert not user_registeration.validate_first_name("harsh")
@@ -12,14 +29,15 @@ def test_last_name():
     assert user_registeration.validate_first_name("Jain")
 
 
-def test_email():
-    assert not user_registeration.validate_email("jaja.com")
-    assert user_registeration.validate_email("abc.xyz@bl.co.in")
+@pytest.mark.parametrize("email,expected", list_of_emails)
+def test_email(email, expected):
+    assert user_registeration.validate_email(email) == expected
 
 
 def test_phone():
     assert not user_registeration.validate_phone("789 456 1232")
     assert user_registeration.validate_phone("+91 9874561230")
+
 
 def test_password():
     assert not user_registeration.validate_password("okay1@34")
