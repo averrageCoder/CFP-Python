108b4134ad93e8c9e193a4e145c1781cbb660e44
Harsh R Jain <rjainharsh@gmail.com>
2021-11-13 08:36:11 +0530

    ADDED: getter and setter methods for class variables

--- contact.py
+++ contact.py
@@ -20,6 +20,80 @@ class Contact:
         self.phone = phone
         self.email = email
 
+    # a getter function
+    @property
+    def first_name(self):
+        return self._first_name
+
+    # a setter function
+    @first_name.setter
+    def first_name(self, first_name):
+        self._first_name = first_name
+
+    @property
+    def last_name(self):
+        return self._last_name
+
+    # a setter function
+    @last_name.setter
+    def last_name(self, last_name):
+        self._last_name = last_name
+
+    @property
+    def address(self):
+        return self._address
+
+    # a setter function
+    @address.setter
+    def address(self, address):
+        self._address = address
+
+    @property
+    def city(self):
+        return self._city
+
+    # a setter function
+    @city.setter
+    def city(self, city):
+        self._city = city
+
+    # a getter function
+    @property
+    def state(self):
+        return self._state
+
+    # a setter function
+    @state.setter
+    def state(self, state):
+        self._state = state
+
+    @property
+    def zip_code(self):
+        return self._zip_code
+
+    # a setter function
+    @zip_code.setter
+    def zip_code(self, zip_code):
+        self._zip_code = zip_code
+
+    @property
+    def phone(self):
+        return self._phone
+
+    # a setter function
+    @phone.setter
+    def phone(self, phone):
+        self._phone = phone
+
+    @property
+    def email(self):
+        return self._email
+
+    # a setter function
+    @email.setter
+    def email(self, email):
+        self._email = email
+
     def __str__(self):
         """
         To return a string version of the object
