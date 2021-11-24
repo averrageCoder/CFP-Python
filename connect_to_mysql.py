b5c50726eae8a60338fd35be480ff66cbc11a66e
Harsh R Jain <rjainharsh@gmail.com>
2021-11-24 08:04:58 +0530

    MODIFIED: returning the connection to mysql

--- connect_to_mysql.py
+++ connect_to_mysql.py
@@ -3,6 +3,14 @@ from mysql.connector import connect, Error
 
 
 def get_connection(HOST, USERNAME, PASSWORD, DATABASE):
+    """
+    function to create and return the connection to database
+    :param HOST: HOST name of the database
+    :param USERNAME:  username to use in the database
+    :param PASSWORD: password for the username
+    :param DATABASE: database to be used
+    :return:
+    """
     try:
         connection = connect(
             host=HOST,
