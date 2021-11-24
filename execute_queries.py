1a52c4980af74fa5d8b08b23883ac5f2e071a1c8
Harsh R Jain <rjainharsh@gmail.com>
2021-11-24 23:24:45 +0530

    ADDED: ability to put csv into db and log errors

--- execute_queries.py
+++ execute_queries.py
@@ -1,8 +1,11 @@
 from connect_to_mysql import get_connection
 from dotenv import dotenv_values
+import pandas as pd
+import logging
+logging.basicConfig(filename='mysql.log', filemode='a', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
 
 from sql_queries import create_movies_table_query, create_reviewers_table_query, create_ratings_table_query, \
-    insert_reviewers_query, show_table_query
+    insert_reviewers_query, show_table_query, create_books_table_query
 
 config = dotenv_values(".env")
 HOST = config["HOST"]
@@ -13,17 +16,28 @@ DATABASE = config['DATABASE']
 
 class MySQLUtil:
     def __init__(self, HOST, USERNAME, PASSWORD, DATABASE):
-        self.connection = get_connection(HOST, USERNAME, PASSWORD, DATABASE)
+        try:
+            self.connection = get_connection(HOST, USERNAME, PASSWORD, DATABASE)
+            logging.info("Database connection successful!")
+        except Exception as e:
+            logging.error(str(e))
         self.cursor = self.connection.cursor()
 
-    def execute_query(self, query):
+    def execute_query(self, query, values = None):
         """
         to execute query to database
         :param query: sql query to be executed
         :return:
         """
-        self.cursor.execute(query)
-        self.connection.commit()
+        try:
+            if not values:
+                self.cursor.execute(query)
+            else:
+                self.cursor.execute(query, values)
+            self.connection.commit()
+        except Exception as e:
+            logging.error("{} for query '{}' values '{}'".format(str(e), query, values))
+
 
     def executemany_query(self, query, values):
         """
@@ -32,8 +46,12 @@ class MySQLUtil:
         :param values: values to be used in the query
         :return:
         """
-        self.cursor.executemany(query, values)
-        self.connection.commit()
+        try:
+            self.cursor.executemany(query, values)
+            self.connection.commit()
+        except Exception as e:
+            logging.error("{} for query '{}'".format(str(e), query))
+            self.connection.rollback()
 
     def execute_and_fetch_query(self, query):
         """
@@ -41,17 +59,56 @@ class MySQLUtil:
         :param query: query to be executed and fetched
         :return:
         """
-        self.cursor.execute(query)
-        result = self.cursor.fetchall()
-        self.connection.commit()
-        return result
+        try:
+            self.cursor.execute(query)
+            result = self.cursor.fetchall()
+            return result
+        except Exception as e:
+            logging.error("{} for query '{}'".format(str(e), query))
+            self.connection.rollback()
+            return None
+
+    def read_csv_and_insert_to_db(self, csvfile, table_name):
+        """
+        to read csv and insert to db
+        :param csvfile: path of csv file
+        :param table_name: name of the table to be inserted into
+        :return:
+        """
+        df = pd.read_csv(csvfile)
+        # logging.debug(df.info())
+        columns = df.columns.values.tolist()
+        all_rows = []
+
+        columns_str = ''
+        value_format_specifier = ''
+        for column in columns:
+            columns_str += column + ','
+            value_format_specifier += '%s,'
+        columns_str = columns_str.rsplit(',', 1)[0]
+        value_format_specifier = value_format_specifier.rsplit(',', 1)[0]
+
+        for index, row in df.iterrows():
+            row_value = ()
+            for column in columns:
+                if pd.isna( row[column]):
+                    row[column] = 'NULL'
+                row_value += (row[column], )
+            all_rows.append(row_value)
+            query = "insert into {}({}) values({})".format(table_name, columns_str, value_format_specifier)
+            # print(query)
+            self.execute_query(query, row_value)
 
     def close_connection(self):
         """
         function that closes the connection to mysql
         :return:
         """
-        self.connection.close()
+        try:
+            self.connection.close()
+        except Exception as e:
+            self.connection.rollback()
+            logging.error(str(e))
 
 
 if __name__ == "__main__":
@@ -59,8 +116,11 @@ if __name__ == "__main__":
     mysql_util.execute_query(create_movies_table_query)
     mysql_util.execute_query(create_reviewers_table_query)
     mysql_util.execute_query(create_ratings_table_query)
+    mysql_util.execute_query(create_books_table_query)
     result = mysql_util.execute_and_fetch_query(show_table_query)
     for row in result:
         print(row)
     mysql_util.executemany_query(insert_reviewers_query["query"], insert_reviewers_query["values"])
+    # mysql_util.read_csv_and_insert_to_db("reviewers_data.csv", "reviewers")
+    mysql_util.read_csv_and_insert_to_db("book.csv", "books")
     mysql_util.close_connection()
