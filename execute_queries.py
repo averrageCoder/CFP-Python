9dd00b5fba441f5670e658a92a38ea5196db164b
Harsh R Jain <rjainharsh@gmail.com>
2021-11-24 08:06:38 +0530

    MODIFIED: class approch to create mysql util

--- execute_queries.py
+++ execute_queries.py
@@ -1,66 +1,66 @@
 from connect_to_mysql import get_connection
 from dotenv import dotenv_values
 
+from sql_queries import create_movies_table_query, create_reviewers_table_query, create_ratings_table_query, \
+    insert_reviewers_query, show_table_query
+
 config = dotenv_values(".env")
 HOST = config["HOST"]
 USERNAME = config['USERNAME']
 PASSWORD = config['PASSWORD']
 DATABASE = config['DATABASE']
 
+
+class MySQLUtil:
+    def __init__(self, HOST, USERNAME, PASSWORD, DATABASE):
+        self.connection = get_connection(HOST, USERNAME, PASSWORD, DATABASE)
+        self.cursor = self.connection.cursor()
+
+    def execute_query(self, query):
+        """
+        to execute query to database
+        :param query: sql query to be executed
+        :return:
+        """
+        self.cursor.execute(query)
+        self.connection.commit()
+
+    def executemany_query(self, query, values):
+        """
+        to execute many queries at once
+        :param query: query to be executed
+        :param values: values to be used in the query
+        :return:
+        """
+        self.cursor.executemany(query, values)
+        self.connection.commit()
+
+    def execute_and_fetch_query(self, query):
+        """
+        to execute a query and fetch the result from the query
+        :param query: query to be executed and fetched
+        :return:
+        """
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        self.connection.commit()
+        return result
+
+    def close_connection(self):
+        """
+        function that closes the connection to mysql
+        :return:
+        """
+        self.connection.close()
+
+
 if __name__ == "__main__":
-    connection = get_connection(HOST, USERNAME, PASSWORD, DATABASE)
-    create_movies_table_query = """
-    CREATE TABLE if not exists movies(
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        title VARCHAR(100),
-        release_year YEAR(4),
-        genre VARCHAR(100),
-        collection_in_mil INT
-    )
-    """
-    cursor = connection.cursor()
-    cursor.execute(create_movies_table_query)
-    connection.commit()
-
-    create_reviewers_table_query = """
-    CREATE TABLE if not exists reviewers (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        first_name VARCHAR(100),
-        last_name VARCHAR(100)
-    )
-    """
-    cursor.execute(create_reviewers_table_query)
-    connection.commit()
-
-    create_ratings_table_query = """
-    CREATE TABLE if not exists ratings (
-        movie_id INT,
-        reviewer_id INT,
-        rating DECIMAL(2,1),
-        FOREIGN KEY(movie_id) REFERENCES movies(id),
-        FOREIGN KEY(reviewer_id) REFERENCES reviewers(id),
-        PRIMARY KEY(movie_id, reviewer_id)
-    )
-    """
-    cursor.execute(create_ratings_table_query)
-    connection.commit()
-
-    show_table_query = "DESCRIBE movies"
-    cursor.execute(show_table_query)
-    result = cursor.fetchall()
+    mysql_util = MySQLUtil(HOST, USERNAME, PASSWORD, DATABASE)
+    mysql_util.execute_query(create_movies_table_query)
+    mysql_util.execute_query(create_reviewers_table_query)
+    mysql_util.execute_query(create_ratings_table_query)
+    result = mysql_util.execute_and_fetch_query(show_table_query)
     for row in result:
         print(row)
-
-    insert_reviewers_query = """
-    INSERT INTO reviewers
-    (first_name, last_name)
-    VALUES ( %s, %s )
-    """
-    reviewers_records = [
-        ("Chaitanya", "Baweja"),
-        ("Mary", "Cooper"),
-        ("Marlon", "Crafford"),
-    ]
-
-    cursor.executemany(insert_reviewers_query, reviewers_records)
-    connection.commit()
+    mysql_util.executemany_query(insert_reviewers_query["query"], insert_reviewers_query["values"])
+    mysql_util.close_connection()
